c64a17a6a9f28ec21536fe865aaff6579ecfe397
Joseph Simpson <66641176+jsimpson2020@users.noreply.github.com>
2025-05-08 13:38:13 -0700

    Add files via upload

---
+++ two_sphere_images_linearity.m
@@ -0,0 +1,206 @@
+clc; clear all; close all;
+
+% Compute KM image for data from dielectric spheres. Then compute the
+% modified KM image using the subtraction method. All units are with
+% respect to meters (m) and seconds (s).
+%
+% This is the code used to generate the images in the manuscript,
+% "Detecting, locating and spectrally characterizing targets with SAR," by
+% A. D. Kim, J. Simpson, and C. Tsogka. The parameters in the code below
+% are those used to generate Fig. 6 in that manuscript.
+%
+% Written by J. Simpson on 5/8/2025
+
+%% Imaging System Parameters
+
+% bandwidth
+
+k_num = 26;                     % number of frequency samples
+
+B = 622e6;                      % system bandwidth (Hz)
+c = 3e8;                        % wave speed (m/s)
+f0 = 9.6e9;                     % central frequency (Hz)
+k0 = 2*pi*f0/c;                 % central wavenumber (m^-1)
+dk = pi*B/c;                    % maximum wavenumber distance (m^-1)
+
+k = linspace(k0-dk,k0+dk,k_num);% wavenumber array
+
+lambda0 = 0.0312;               % central wavelength
+
+% flight path
+
+L = 8120;                       % distance from array to imaging window (m)
+R = 3550;                       % range offset (m)
+H = 7300;                       % height (m)
+a = 130;                        % aperture length (m)
+
+array_size = 32;                % number of spatial samples
+
+array = zeros(3,array_size);    % flight path array
+
+for j = 1:array_size
+    array(1,j)=(a*(2*j-array_size-1))/(2*array_size-2);
+end
+array(2,:) = ones(1,array_size)*(R);
+array(3,:) = ones(1,array_size)*(H);
+
+%% SAR data simulation
+
+% target parameters
+
+nrel1 = 1.2;                            % refractive index of sphere 1
+radius1 = 2*lambda0;                    % radius of sphere 1
+center1 = [50*lambda0;-25*lambda0;0];   % center of sphere 1
+
+nrel2 = 1.8;                            % refractive index of sphere 2
+radius2 = 1.8*lambda0;                  % radius of sphere 2
+center2 = [-30*lambda0;30*lambda0;0];   % center of sphere 2
+
+% MFS parameters for sphere 1
+
+delta_s1 = 0.4*radius1;
+delta_int1 = 0.4*radius1;
+points1 = 400;
+
+% MFS parameters for sphere 2
+
+delta_s2 = 0.4*radius2;
+delta_int2 = 0.4*radius2;
+points2 = 600;
+
+% MFS for SAR data
+
+[bdy,normal] = twospheres(radius1,radius2,center1,center2,points1,points2);
+
+nrel1_array = nrel1*ones(1,points1);
+nrel2_array = nrel2*ones(1,points2);
+nrel = [nrel1_array,nrel2_array];
+
+delta_s1_array = delta_s1*ones(1,points1);
+delta_s2_array = delta_s2*ones(1,points2);
+delta_s = [delta_s1_array,delta_s2_array];
+
+delta_int1_array = delta_int1*ones(1,points1);
+delta_int2_array = delta_int2*ones(1,points2);
+delta_int = [delta_int1_array,delta_int2_array];
+
+data = SAR_data_MFS_3D(bdy,normal,nrel,k,array,delta_s,delta_int);
+
+%% Add measurement noise
+  
+sn     = 0.1; % this is the parameter to change
+B0     = data;
+enr    = sqrt(sum(abs(data(:)).^2)/length(data(:)));
+Bnoise = enr*sn*(randn(size(data))+1i*randn(size(data)))/sqrt(2);
+BB     = B0 + Bnoise;
+
+SNR_est       = -10 * log10( sn );
+SNR_real      =  10 * log10( norm(data) / norm(Bnoise) );
+   
+disp( ' ' );
+disp( '   ---' );
+disp( '   SNR' );
+disp( '   ---' );
+disp( ' ' );
+disp( ['   Realization SNR = ', num2str(SNR_real), ' dB' ] );
+disp( ['   Average SNR     = ', num2str(SNR_est), ' dB'  ] );
+disp( ' ' );
+
+%% KM image computation
+
+search_x = linspace(-500/k0,500/k0,201);
+
+[X, Y] = meshgrid(search_x);
+
+I_KM = 0*X;
+
+for m = 1:k_num
+    for n = 1:array_size
+        dist = sqrt((X-array(1,n)).^2+(Y-array(2,n)).^2+(array(3,n)).^2);
+        I_KM = I_KM + data(m,n)*exp(-1i.*2.*k(m).*dist);
+    end
+end
+
+I_KM_norm = abs(I_KM)./max(abs(I_KM(:)));
+
+K = abs(I_KM_norm);
+[max_num,max_idx] = max(K(:));
+[x_KM,y_KM]=ind2sub(size(K),max_idx);
+
+s = linspace(0,2*pi,21);
+
+% KM image plotting
+
+figure('DefaultAxesFontSize',24)
+fig = pcolor(k0*search_x,k0*search_x,I_KM_norm);
+set(fig,'EdgeColor','None');
+xlabel('$k_0x$', 'Interpreter','Latex');
+ylabel('$k_0y$', 'Interpreter','Latex');
+colorbar
+axis square
+hold on
+plot(k0*search_x(y_KM),k0*search_x(x_KM),'r+','LineWidth',1.5); %peak location
+plot(k0*center1(1),k0*center1(2),'rx','LineWidth',1.5);
+plot(k0*center1(1)+k0*radius1*cos(s),k0*center1(2)+k0*radius1*sin(s),'w:',...
+    'LineWidth',2);
+plot(k0*center2(1),k0*center2(2),'rx','LineWidth',1.5);
+plot(k0*center2(1)+k0*radius2*cos(s),k0*center2(2)+k0*radius2*sin(s),'w:',...
+    'LineWidth',2);
+hold off
+% print('-depsc', 'two_sphere_images_linearity1.eps');
+% print('-dpdf', 'two_sphere_images_linearity1.pdf');
+
+%% KM Linearity Subtraction Method
+
+Dprime = zeros(k_num,array_size);
+tar_loc = [search_x(y_KM);search_x(x_KM);0];
+
+for i = 1:k_num
+    for j = 1:array_size
+        Dprime(i,j) = exp(2*1i*k(i)*vecnorm(array(:,j)-tar_loc,2))/...
+            (vecnorm(array(:,j)-tar_loc,2))^2;
+    end
+end
+
+I_KM_prime = 0*X;
+
+for m = 1:length(k)
+    for n = 1:array_size
+        R = sqrt((X-array(1,n)).^2+(Y-array(2,n)).^2+(array(3,n)).^2);
+        I_KM_prime = I_KM_prime + Dprime(m,n)*exp(-1i.*2.*k(m).*R);
+    end
+end
+
+I_KM_prime = abs(I_KM_prime)./max(abs(I_KM_prime(:)));
+
+I_KM_2 = I_KM_norm - I_KM_prime;
+
+I_KM_2_norm = I_KM_2./max(abs(I_KM_2(:)));
+
+
+% Modified KM image plotting
+
+K = I_KM_2_norm;
+[max_num,max_idx] = max(K(:));
+[x,y]=ind2sub(size(K),max_idx);
+
+figure('DefaultAxesFontSize',24)
+fig = pcolor(k0*search_x,k0*search_x,I_KM_2_norm);
+set(fig,'EdgeColor','None');
+xlabel('$k_0x$', 'Interpreter','Latex');
+ylabel('$k_0y$', 'Interpreter','Latex');
+colorbar
+axis square
+hold on
+plot(k0*search_x(y),k0*search_x(x),'r+','LineWidth',1.5); %peak location
+plot(k0*center1(1),k0*center1(2),'rx','LineWidth',1.5);
+plot(k0*center1(1)+k0*radius1*cos(s),k0*center1(2)+k0*radius1*sin(s),'w:',...
+    'LineWidth',2);
+plot(k0*center2(1),k0*center2(2),'rx','LineWidth',1.5);
+plot(k0*center2(1)+k0*radius2*cos(s),k0*center2(2)+k0*radius2*sin(s),'w:',...
+    'LineWidth',2);
+hold off
+% print('-depsc', 'two_sphere_images_linearity2.eps');
+% print('-dpdf', 'two_sphere_images_linearity2.pdf');
+
+
